b9c63a7fe0b1478085faceb0ca808abfbf1cd5c3
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-20 23:14:36 +0300

    Create Section.jsx

---
+++ src/Section/Section.jsx
@@ -0,0 +1,12 @@
+import { Items } from "../Items";
+import { Comments } from "../Comments";
+import "./Section.css";
+
+export const Section = () => {
+  return (
+    <section className="section">
+      <Items />
+      <Comments />
+    </section>
+  );
+};
