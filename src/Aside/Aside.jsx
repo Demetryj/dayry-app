23a5ff9bcdc5dcb0b464fc16a4505c2fdd7c28be
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-20 23:13:19 +0300

    Create Aside.jsx

---
+++ src/Aside/Aside.jsx
@@ -0,0 +1,10 @@
+import "./Aside.css";
+
+export const Aside = () => {
+  return (
+    <aside className="aside">
+      <h2 className="aside_title">DAYRY APP</h2>
+      <p className="aside_text">Comment whit no sense</p>
+    </aside>
+  );
+};
