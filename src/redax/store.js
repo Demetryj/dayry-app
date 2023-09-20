42e9c679fb7ee0eefef0576a449e5bc4e497c8ac
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-20 23:14:29 +0300

    Create store.js

---
+++ src/redax/store.js
@@ -0,0 +1,33 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { commentReducer } from "./comment/commentSlice";
+
+const authPersistConfig = {
+  key: "comment",
+  storage,
+};
+
+export const store = configureStore({
+  reducer: {
+    comments: persistReducer(authPersistConfig, commentReducer),
+  },
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  ],
+});
+
+export const persistor = persistStore(store);
