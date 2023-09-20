3160ec39aa056478e90c683dd316ac8d242915a6
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-20 23:13:43 +0300

    Create Comment.jsx

---
+++ src/Comment/Comment.jsx
@@ -0,0 +1,13 @@
+import "./Comment.css";
+
+export const Comment = () => {
+  return (
+    <div className="comment_wrapper">
+      <div
+        className="comment_color"
+        //   style={{ background-color: data.color }}
+      ></div>
+      <span className="comment_text"></span>
+    </div>
+  );
+};
