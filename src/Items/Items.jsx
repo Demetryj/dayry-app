d7ad87c0b6388a0e9b877aa1090a78900abf624f
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-21 14:20:24 +0300

    add handling adding of active item and rename function for handling submiting form

--- src/Items/Items.jsx
+++ src/Items/Items.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Item } from "../Item";
 import { selectItems } from "../redax/comment/selectors";
-import { addItem } from "../redax/comment/commentSlice";
+import { addItem, changeActiveItem } from "../redax/comment/commentSlice";
 import { generateRandomNumber } from "../utils/generateRandomNumber";
 import "./Items.css";
 
@@ -9,31 +9,37 @@ export const Items = () => {
   const dispatch = useDispatch();
   const allItems = useSelector(selectItems);
 
-  const handleAddButtonClick = (event) => {
+  const handleSubmitForm = (event) => {
     event.preventDefault();
-    dispatch(
-      addItem({
-        id: generateRandomNumber(),
-        name: event.target.item.value,
-        comments: [],
-      })
-    );
+    const newAtem = {
+      id: generateRandomNumber(),
+      name: event.target.item.value,
+      comments: [],
+    };
+    dispatch(addItem(newAtem));
+
+    if (allItems.length === 0) {
+      dispatch(changeActiveItem(newAtem));
+    }
 
     event.target.reset();
   };
+
+  const handleItemClick = (item) => dispatch(changeActiveItem(item));
 
   return (
     <div>
       <div className="items">
         <h2 className="items_title">Items</h2>
 
-        <form className="items-form" onSubmit={handleAddButtonClick}>
+        <form className="items-form" onSubmit={handleSubmitForm}>
           <input
             className="items-form_input"
             type="text"
             name="item"
             placeholder="Type name here..."
             required
+            autoComplete="off"
           />
           <button className="items-form_button" type="submit">
             Add New
@@ -43,7 +49,11 @@ export const Items = () => {
         {allItems.length > 0 && (
           <ul className="items_list">
             {allItems.map((item) => (
-              <li className="item" key={item.id}>
+              <li
+                className="item"
+                key={item.id}
+                onClick={() => handleItemClick(item)}
+              >
                 <Item data={item} />
               </li>
             ))}
