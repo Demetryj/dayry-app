eae4c8fd64ce6a2a85b9977cad077993288d6c2b
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-21 14:11:45 +0300

    add form`s submit and comments` list render

--- src/Comments/Comments.jsx
+++ src/Comments/Comments.jsx
@@ -1,33 +1,58 @@
-import { Comment } from "../Comment";
-import "./Comments.css";
+import { useSelector, useDispatch } from 'react-redux';
+import { selectActiveItem } from '../redax/comment/selectors';
+import { addComment } from '../redax/comment/commentSlice';
+import { Comment } from '../Comment';
+import './Comments.css';
 
 export const Comments = () => {
+  const activeItem = useSelector(selectActiveItem);
+  const dispatch = useDispatch();
+
+  const handleSubmitForm = event => {
+    event.preventDefault();
+
+    const {
+      elements: { comment, color },
+    } = event.currentTarget;
+
+    dispatch(
+      addComment({ body: comment.value, color: color.value, id: activeItem.id })
+    );
+
+    event.currentTarget.reset();
+  };
+
   return (
     <div>
       <div className="comments">
-        <h2 className="comments_title">Comments #78965412</h2>
+        <h2 className="comments_title">
+          Comments #{activeItem ? activeItem?.id : ''}
+        </h2>
 
-        <ul className="comments_list">
-          <li>
-            <Comment />
-          </li>
-        </ul>
+        {activeItem.comments.length > 0 && (
+          <ul className="comments_list">
+            {activeItem.comments.map((comment, index) => (
+              <li key={index}>
+                <Comment comment={comment} />
+              </li>
+            ))}
+          </ul>
+        )}
 
-        <form className="comments-form">
+        <form className="comments-form" onSubmit={handleSubmitForm}>
           <input className="comments-form_input" type="color" name="color" />
           <textarea
             className="comments-form_textarea"
             type="text"
-            name="comments"
+            name="comment"
             placeholder="Type comment here..."
             required
+            autoComplete="off"
           />
-          <button className="comments-form_button">Add New</button>
+          <button className="comments-form_button" type="submit">
+            Add New
+          </button>
         </form>
-
-        <ul className="comments_list">
-          <li></li>
-        </ul>
       </div>
     </div>
   );
