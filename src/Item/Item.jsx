b208400e0546218cb803c949001f11c89d2b2b66
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-21 14:18:07 +0300

    add handling deleting of item

--- src/Item/Item.jsx
+++ src/Item/Item.jsx
@@ -1,14 +1,28 @@
-import { useDispatch } from "react-redux";
-import { deleteItem } from "../redax/comment/commentSlice";
-import "./Item.css";
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteItem } from '../redax/comment/commentSlice';
+import { selectActiveItem, selectItems } from '../redax/comment/selectors';
+import { changeActiveItem } from '../redax/comment/commentSlice';
+import './Item.css';
 
 export const Item = ({ data: { id, name, comments } }) => {
   const dispatch = useDispatch();
+  const activeItem = useSelector(selectActiveItem);
+  const allItems = useSelector(selectItems);
 
-  const handleButtonDeleteClick = () => dispatch(deleteItem(id));
+  const handleButtonDeleteClick = () => {
+    dispatch(deleteItem(id));
+
+    if (activeItem.id === id) {
+      dispatch(changeActiveItem(allItems[1]));
+    }
+  };
 
   return (
-    <div className="item_wrapper">
+    <div
+      className={
+        activeItem.id === id ? 'item_wrapper active-item' : 'item_wrapper'
+      }
+    >
       <p className="item_text">{name}</p>
       <span className="item_count">{comments.length}</span>
       <button
