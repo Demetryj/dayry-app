460593811c90ee1b447730f3c9095d0a155a5efa
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-21 14:12:56 +0300

    add reducer for adding comment

--- src/redax/comment/commentSlice.js
+++ src/redax/comment/commentSlice.js
@@ -1,7 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 export const commentSlice = createSlice({
-  name: "comment",
+  name: 'comment',
   initialState: {
     items: [],
     activeItem: null,
@@ -11,15 +11,22 @@ export const commentSlice = createSlice({
       state.items.push(action.payload);
     },
     deleteItem(state, action) {
-      const index = state.items.findIndex((item) => item.id === action.payload);
+      const index = state.items.findIndex(item => item.id === action.payload);
 
       state.items.splice(index, 1);
     },
     changeActiveItem(state, action) {
       state.activeItem = action.payload;
     },
+    addComment(state, action) {
+      const item = state.items.find(item => item.id === action.payload.id);
+      item.comments.push(action.payload);
+
+      state.activeItem.comments.push(action.payload);
+    },
   },
 });
 
-export const { addItem, deleteItem, method } = commentSlice.actions;
+export const { addItem, deleteItem, changeActiveItem, addComment } =
+  commentSlice.actions;
 export const commentReducer = commentSlice.reducer;
